21a1a52cf14d40f55ac94b307237d0fa4e74db49
eduardosiilvaa <eduardojunio2004@gmail.com>
2024-08-30 07:43:20 -0300

    Atualizar o data_e_hora_atual.php

---
+++ data_e_hora_atual.php
@@ -0,0 +1,10 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// Obtém a data e a hora
+$horaAtual = date('d/m/Y H:i:s');
+
+// Exibe a data e a hora
+echo "A data e hora atuais são: $horaAtual";
+?>
